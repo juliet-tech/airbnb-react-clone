9a31a16a1796a978109fe0c1f305cc98de9d41f0
Juliettech <juliettech.1393@gmail.com>
2019-04-16 14:53:38 -0500

    adding flat component with html and css. missing content inside the flats

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+    const flat = {
+      "id": 148,
+      "name": "Trendy Apt in Buttes Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+      "price": 200,
+      "priceCurrency": "EUR",
+      "lat": 48.885707,
+      "lng": 2.343543
+    };
+
+    return(
+      <div className='app'>
+        <div className='main'>
+          <div className='search'>
+          </div>
+          <div className='flats'>
+          </div>
+        </div>
+        <div className='map'>
+        </div>
       </div>
     );
+
+
+
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,21 @@ import * as serviceWorker from './serviceWorker';
 
 import Hello from './components/hello';
 
-const bye = <div><Hello firstName='Nonica' lastName='Chevalier' />
-              <Hello firstName='Monca' lastName='Chevalier' /></div>;
+// const bye = <div>
+//               <Hello firstName='Nonica' lastName='Chevalier' />
+//               <Hello firstName='Monca' lastName='Chevalier' />
+//             </div>;
 
-ReactDOM.render(<Hello firstName='Juliette' lastName='Chevalier' />, document.getElementById('root'));
-ReactDOM.render(bye, document.getElementById('root')); // Can also do this and it will work. Because these are both addressing the element #root, only the last one gets executed
+// ReactDOM.render(<Hello firstName='Juliette' lastName='Chevalier' />, document.getElementById('root'));
+// ReactDOM.render(bye, document.getElementById('root')); // Can also do this and it will work. Because these are both addressing the element #root, only the last one gets executed
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+
